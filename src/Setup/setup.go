d65ab404301849d38627c4ea031a4a220847fb89
Manish <kumarmanish19963@gmail.com>
2021-09-14 11:21:16 +0530

    Set Commit

--- src/Setup/setup.go
+++ src/Setup/setup.go
@@ -57,3 +57,72 @@ func Pedersen_ver(ck []*big.Int, c, u, r *big.Int) int {
 	}
 	return 0
 }
+
+func Setup_Set(lambda int, neu int) ( n, f *big.Int)  {
+  pk, _ := rsa.GenerateKey(rand.Reader , lambda)
+  var F *big.Int
+
+  for  {
+    F, _ = rand.Int(rand.Reader, pk.PublicKey.N)
+    if F!=pk.Primes[0] && F!=pk.Primes[1] && F!= big.NewInt(1) {
+        break
+      }
+}
+
+G := F.Exp(F, F, pk.PublicKey.N)
+N := pk.PublicKey.N
+
+return N, G
+}
+
+
+func fu(x *big.Int) *big.Int {
+  temp1 :=big.NewInt(0)
+  temp1.Add(x,big.NewInt(2))
+  temp1.Mul(temp1,big.NewInt(2) )
+  temp2 :=big.NewInt(0)
+  temp2.Add(x,big.NewInt(1))
+  temp2.Mul(temp2, temp2)
+
+//2*(x+2)*
+  fx := math.Log2(temp2)
+
+  return fx
+
+}
+
+
+
+//Function to map the set element to prime
+
+func Hprime(u *big.Int) *big.Int {
+
+
+  for j:= add(fu(u),1) ; j< fu(add(u,1) ; j++ {
+
+    x := fu(u)+j
+    if x.ProbablyPrime(10) == true {
+      break
+    }
+    return x
+  }
+
+
+}
+
+//Set Commitment function
+
+func Commit(N, G, U[] *big.Int) (c, o *big.Int)  {
+
+  var P [] *big.Int
+  var Acc *big.Int
+  for i :=0; i< len(U); i++ {
+
+    u := U[i]
+    P[i] = Hprime(u)
+    Acc *= G.Exp(G, P[i], pk.PublicKey.N)
+    Acc.Exp(Acc, 1, pk.PublicKey.N)
+   }
+   return Acc, nil
+
+}
