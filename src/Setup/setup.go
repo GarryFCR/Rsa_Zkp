1aed310e735d09fd88b8b0f266a59bea266b2f83
GarryFCR <garryranee@gmail>
2021-09-16 15:12:05 +0530

    Root protocol

--- src/Setup/setup.go
+++ src/Setup/setup.go
@@ -10,20 +10,26 @@ import (
 
 func Pedersen_setup(lambda, nu int64) (x, y, z *big.Int) {
 	i := 0
-	var prime_bound1, prime_bound2 big.Int
+	var prime_bound1, prime_bound2 *big.Int
 	var prime *big.Int
 
-	upper := big.NewInt(int64(math.Pow(2, float64(nu))))
-	lower := big.NewInt(int64(math.Pow(2, float64(nu-1))))
+	//upper := big.NewInt(int64(math.Pow(2, float64(nu))))
+	//lower := big.NewInt(int64(math.Pow(2, float64(nu-1))))
+
+	upper := big.NewInt(2)
+	upper.Exp(upper, big.NewInt(nu), nil)
+	lower := big.NewInt(2)
+	lower.Exp(lower, big.NewInt(nu-1), nil)
 
 	for i == 0 {
 		prime, _ = rand.Prime(rand.Reader, int(lambda))
-		fmt.Println(prime)
-		prime_bound1.Sub(upper, prime)
-		prime_bound2.Sub(prime, lower)
+
+		prime_bound1 = new(big.Int).Sub(upper, prime)
+		prime_bound2 = new(big.Int).Sub(prime, lower)
 		if prime_bound1.Sign() == 1 && prime_bound2.Sign() == 1 {
 			break
 		}
+		fmt.Println(prime, prime_bound1.Sign(), prime_bound2.Sign())
 	}
 	//fmt.Println(prime)
 	g, _ := rand.Int(rand.Reader, prime)
