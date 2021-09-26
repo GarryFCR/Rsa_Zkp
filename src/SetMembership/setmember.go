7eab33ef042d66242e4318c561a7662baf20d56a
GarryFCR <garryranee@gmail.com>
2021-09-26 14:54:41 +0530

    Integration of sub protocols in SetMembership

--- src/SetMembership/setmember.go
+++ src/SetMembership/setmember.go
@@ -3,11 +3,13 @@ package SetMembership
 import (
 	"crypto/rand"
 	"math/big"
-	/*	hashEq "../HashEq"
-		hash2prime "../Hashtoprime"
-		root "../Root"
-		pedersen "../Setup"
-		modEq "../modEq"*/)
+
+	hashEq "../HashEq"
+	hash2prime "../Hashtoprime"
+	root "../Root"
+	pedersen "../Setup"
+	modEq "../modEq"
+)
 
 func KeyGen(ck []big.Int, p, q big.Int) []big.Int {
 
@@ -26,46 +28,51 @@ func KeyGen(ck []big.Int, p, q big.Int) []big.Int {
 
 }
 
-/*
-func Prove(crs []*big.Int, Cu, cu, U, u, ru *big.Int) (Ce, ce *big.Int, proof_root, proof_mod, proof_hash []*big.Int) {
+func Prove(crs []big.Int, Cu, cu, u, ru, p, q big.Int) (Ce, ce big.Int, proof_root, proof_mod, proof_hash []big.Int) {
 
 	e := hash2prime.Hprime(u)
-	j := new(big.Int).Sub(e, hash2prime.Fu(u))
-	ce, rq := pedersen.Pedersen_commit(crs[3:], crs[2], e)
-	Ce, r := pedersen.Pedersen_commit(crs[1:3], crs[0], e)
-	inverse := new(big.Int).ModInverse(e, crs[0])
-	W := new(big.Int).Exp(Cu, inverse, crs[0])
+	fu := hash2prime.Fu(u)
+	j := new(big.Int).Sub(&e, &fu)
+	ce, rq := pedersen.Pedersen_commit(crs[3:], crs[3], e)
+	Ce, r := pedersen.Pedersen_commit(crs[:3], crs[0], e)
 
-	commit := []*big.Int{Ce, Cu}
-	root_witness := []*big.Int{e, r, W}
+	phi := new(big.Int).Mul(new(big.Int).Sub(&q, big.NewInt(1)), new(big.Int).Sub(&p, big.NewInt(1)))
+
+	inverse := new(big.Int).ModInverse(&e, phi)
+	W := new(big.Int).Exp(&Cu, inverse, &crs[0])
+
+	commit := []big.Int{Ce, Cu}
+	root_witness := []big.Int{e, r, *W}
 	pi_root := root.Prove(crs[:3], commit, root_witness, int64(32), int64(32), int64(32))
 
-	commit1 := []*big.Int{Ce, ce}
-	mod_witness := []*big.Int{e, e, r, rq}
+	commit1 := []big.Int{Ce, ce}
+	mod_witness := []big.Int{e, e, r, rq}
 	pi_mod := modEq.Prove(crs, commit1, mod_witness, int64(64))
 
-	hash_commitment := []*big.Int{ce, cu}
-	hash_witness := []*big.Int{e, u, rq, ru, j}
-	pi_hash := hashEq.Prove(crs[4:], hash_witness, hash_commitment, int64(32), int64(32), int64(32), int64(32))
+	hash_commitment := []big.Int{ce, cu}
+	hash_witness := []big.Int{e, u, rq, ru, *j}
+	pi_hash := hashEq.Prove(crs[3:], hash_commitment, hash_witness, int64(32), int64(32), int64(32), int64(32))
 
 	return Ce, ce, pi_root, pi_mod, pi_hash
 
 }
 
-func VerProof(crs []*big.Int, Cu, cu *big.Int, Ce, ce *big.Int, pi_root, pi_mod, pi_hash []*big.Int) int {
+func VerProof(crs []big.Int, Cu, cu big.Int, Ce, ce big.Int, pi_root, pi_mod, pi_hash []big.Int) int {
 
-	commit := []*big.Int{Ce, Cu}
+	commit := []big.Int{Ce, Cu}
 	root_bool := root.VerProof(crs[:3], commit, pi_root, int64(32), int64(32), int64(32))
-	commit1 := []*big.Int{Ce, ce}
+
+	commit1 := []big.Int{Ce, ce}
 	modEq_bool := modEq.VerProof(crs, commit1, pi_mod)
 
-	hash_commitment := []*big.Int{ce, cu}
-	hash_bool := hashEq.VerProof(crs[4:], hash_commitment, pi_hash)
+	hash_commitment := []big.Int{ce, cu}
+	hash_bool := hashEq.VerProof(crs[3:], hash_commitment, pi_hash)
 
+	//fmt.Println(root_bool, modEq_bool, hash_bool)
 	if root_bool == 1 && modEq_bool == 1 && hash_bool == 1 {
 		return 1
 	}
 
 	return 0
 
-}*/
+}
--- src/main.go
+++ src/main.go
@@ -4,86 +4,97 @@ import (
 	"fmt"
 	"math/big"
 
-	HashEq "./HashEq"
-	hash2prime "./Hashtoprime"
-	root "./Root"
-	setm "./SetMembership"
+	member "./SetMembership"
 	setup "./Setup"
-	mod "./modEq"
 )
 
 func main() {
 
-	//PEDERSEN COMMITMENT-----------------------------------------
+	//SETUP------------------------------------------------------------------
+	//PEDERSEN COMMITMENT----------------------------------------------------
 	prime, g, h := setup.Pedersen_setup(12, 12)
-	ck := []big.Int{prime, g, h}
+	ck_pedersen := []big.Int{prime, g, h}
 	u := big.NewInt(12345)
 
-	c, o := setup.Pedersen_commit(ck, prime, *u)
+	cu, ru := setup.Pedersen_commit(ck_pedersen, prime, *u)
 
-	ver := setup.Pedersen_ver(ck, c, *u, o)
+	ver := setup.Pedersen_ver(ck_pedersen, cu, *u, ru)
 	if ver == 1 {
 		fmt.Println("Pedersen:Commitment VERIFIED")
 	}
 
-	//SET COMMITMENT--------------------------------------------
+	//SET COMMITMENT----------------------------------------------------------
 	N, G, p, q := setup.Set_setup(12, 12)
 
-	ck1 := []big.Int{N, G}
+	ck_set := []big.Int{N, G}
 	set := []big.Int{*big.NewInt(12342), *big.NewInt(12343), *big.NewInt(12344), *big.NewInt(12345)}
 
-	com, _ := setup.Set_commit(ck1, set)
+	Acc, _ := setup.Set_commit(ck_set, set)
 
-	ver1 := setup.Set_ver(ck1, set, com)
+	ver1 := setup.Set_ver(ck_set, set, Acc)
 	if ver1 == 1 {
 		fmt.Println("Set commit:Commitment VERIFIED")
 	}
 
-	//Setmembership(keygen)--------------------------------------------------------------------
+	//SETMEMBERSHIP------------------------------------------------------------
+	//KEYGEN-------------------------------------------------------------------
 	ck_key := []big.Int{N, G, prime, g, h}
-	crs := setm.KeyGen(ck_key, p, q)
+	crs := member.KeyGen(ck_key, p, q)
+
+	//PROVE--------------------------------------------------------------------
+
+	Ce, ce, pi_root, pi_mod, pi_hash := member.Prove(crs, Acc, cu, *u, ru, p, q)
+
+	//VERIFICATION--------------------------------------------------------------
+
+	BOOL := member.VerProof(crs, Acc, cu, Ce, ce, pi_root, pi_mod, pi_hash)
+
+	if BOOL == 1 {
+		fmt.Println("Verified")
+	}
 
 	//Root-----------------------------------------------------------------------------------
-	e := hash2prime.Hprime(*u)
-	Ce, r := setup.Pedersen_commit(crs[:3], N, e)
+	//e := hash2prime.Hprime(*u)
+	//Ce, r := setup.Pedersen_commit(crs[:3], N, e)
 
-	phi := new(big.Int).Mul(new(big.Int).Sub(&q, big.NewInt(1)), new(big.Int).Sub(&p, big.NewInt(1)))
-	inverse := new(big.Int).ModInverse(&e, phi)
-	W := new(big.Int).Exp(&com, inverse, &N)
+	//phi := new(big.Int).Mul(new(big.Int).Sub(&q, big.NewInt(1)), new(big.Int).Sub(&p, big.NewInt(1)))
+	//inverse := new(big.Int).ModInverse(&e, phi)
+	//W := new(big.Int).Exp(&com, inverse, &N)
 
-	commit := []big.Int{Ce, com}
-	root_witness := []big.Int{e, r, *W}
+	//commit := []big.Int{Ce, com}
+	//root_witness := []big.Int{e, r, *W}
 
-	pi_root := root.Prove(crs[0:3], commit, root_witness, int64(12), int64(12), int64(12))
+	//pi_root := root.Prove(crs[0:3], commit, root_witness, int64(12), int64(12), int64(12))
 
-	ver2 := root.VerProof(crs[0:3], commit, pi_root, int64(12), int64(12), int64(12))
-	if ver2 == 1 {
-		fmt.Println("Root :Root VERIFIED")
-	}
+	//ver2 := root.VerProof(crs[0:3], commit, pi_root, int64(12), int64(12), int64(12))
+	//if ver2 == 1 {
+	//	fmt.Println("Root :Root VERIFIED")
+	//}
 
 	//ModEq-----------------------------------------------------------------------------------
-	ce, rq := setup.Pedersen_commit(crs[3:], prime, e)
-	commit_mod := []big.Int{Ce, ce}
-	mod_witness := []big.Int{e, e, r, rq}
-	pi_mod := mod.Prove(crs, commit_mod, mod_witness, int64(12))
+	//ce, rq := setup.Pedersen_commit(crs[3:], prime, e)
+	//commit_mod := []big.Int{Ce, ce}
+	//mod_witness := []big.Int{e, e, r, rq}
+	//pi_mod := mod.Prove(crs, commit_mod, mod_witness, int64(12))
 	//fmt.Println(ce)
 
-	ver3 := mod.VerProof(crs, commit_mod, pi_mod)
-	if ver3 == 1 {
-		fmt.Println("ModEq :Modeq VERIFIED")
-	}
+	//ver3 := mod.VerProof(crs, commit_mod, pi_mod)
+	//if ver3 == 1 {
+	//	fmt.Println("ModEq :Modeq VERIFIED")
+	//}
 
 	//HashEq-----------------------------------------------------------------------------------
-	fu := hash2prime.Fu(*u)
-	j := new(big.Int).Sub(&e, &fu)
+	//fu := hash2prime.Fu(*u)
+	//j := new(big.Int).Sub(&e, &fu)
 
-	commit_hash := []big.Int{ce, c}
-	hash_witness := []big.Int{e, *u, rq, o, *j}
+	//commit_hash := []big.Int{ce, c}
+	//hash_witness := []big.Int{e, *u, rq, o, *j}
 
-	pi_hash := HashEq.Prove(crs[3:], commit_hash, hash_witness, int64(12), int64(12), int64(12), int64(12))
+	//	pi_hash := HashEq.Prove(crs[3:], commit_hash, hash_witness, int64(12), int64(12), int64(12), int64(12))
 
-	ver4 := HashEq.VerProof(crs[3:], commit_hash, pi_hash)
-	if ver4 == 1 {
-		fmt.Println("HashEq :hash VERIFIED")
-	}
+	//ver4 := HashEq.VerProof(crs[3:], commit_hash, pi_hash)
+	//if ver4 == 1 {
+	//	fmt.Println("HashEq :hash VERIFIED")
+	//}
+
 }
